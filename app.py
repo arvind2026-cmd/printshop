4962d31f000b3259cf7f80f6d92f9f604524452a
Arvind Kumar <arvindhzb07@gmail.com>
2026-06-28 17:33:31 +0530

    ngrok print fix:

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, send_file, make_response
 import os
+import hmac
+import hashlib
 
 app = Flask(__name__)
+
+RAZORPAY_SECRET = "printshop123"
 
 @app.route('/')
 def home():
@@ -18,17 +22,44 @@ def qr():
 @app.route('/save', methods=['POST'])
 def save():
     file = request.files['photo']
-    file.save('print_me.jpg')
+    file.save('C:\\printshop\\print_me.jpg')
     return 'OK'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Razorpay signature verify karo
+    received_sig = request.headers.get('X-Razorpay-Signature', '')
+    body = request.get_data()
+    
+    expected_sig = hmac.new(
+        RAZORPAY_SECRET.encode(),
+        body,
+        hashlib.sha256
+    ).hexdigest()
+    
+    if hmac.compare_digest(received_sig, expected_sig):
+        data = request.json
+        event = data.get('event', '')
+        print("EVENT:", event)
+        
+        if event == 'payment.captured':
+            print("PAYMENT HO GAYA - PRINT HO RAHA HAI!")
+            try:
+                os.startfile('C:\\printshop\\print_me.jpg', 'print')
+            except Exception as e:
+                print("Print Error:", e)
+        return 'OK', 200
+    else:
+        print("SIGNATURE GALAT HAI!")
+        return 'Unauthorized', 401
 
 @app.route('/print', methods=['POST'])
 def print_file():
     try:
-        os.startfile('print_me.jpg', 'print')
+        os.startfile('C:\\printshop\\print_me.jpg', 'print')
     except:
         pass
     return 'OK'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
